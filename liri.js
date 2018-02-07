7ad981418bedd62fd8e5058b171cb075600c3493
Ariana <ariana.dv4@gmail.com>
2018-02-07 00:09:11 -0600

    console log twitter and spotify info based on user input

--- liri.js
+++ liri.js
@@ -11,56 +11,48 @@ var client = new Twitter(keys.twitter);
 
 var userRequest = process.argv[2];
 
-
 //Spotify App URL
 var spot_apiURL = spotify
-  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-  .then(function(data) {
-   // console.log(data); 
-  });
-
-spotify.search({ type: 'track', query: 'Real Slim Shady', limit: 1})
-	.then(function(response){
-		console.log(response.tracks.items[0].artists[0].name);
-		console.log(response.tracks.items[0].name);
-		console.log(response.tracks.items[0].album.external_urls.spotify);
-		console.log(response.tracks.items[0].album.name);
-	})
-	.catch(function(err){
-		console.log(err);
-	});
-
-  
+  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx');
 
 //Twitter App URL
-// var params = {screen_name: 'Ariana_DV4', count: 20};
-// 	client.get('statuses/user_timeline', params, function (error, tweet_data, response) {
-// 	var tweets = tweet_data.slice();
-			
-// 		for (var i = 0; i < tweets.length; i++) {
-// 			console.log({user: tweets[i].user.name, date_created: tweets[i].created_at, status:tweets[i].text});	  
-// 		}
-// 		console.log(tweets.length);
-// 	});
-	
- 
+var params = {screen_name: 'Ariana_DV4', count: 20}; 
 
 //Recognize user input and execute appropriate request    
-	//switch(userRequest){
+	switch(userRequest){
 
-	//	case "my-tweets":
-	//	console.log("TWEETS");
-	//	break;
+		case "my-tweets":
+			console.log("TWEETS");	
+			client.get('statuses/user_timeline', params, function (error, tweet_data, response) {
+			var tweets = tweet_data.slice();
+			
+			for (var i = 0; i < tweets.length; i++) {
+			console.log({user: tweets[i].user.name, date_created: tweets[i].created_at, status:tweets[i].text});	  
+				}
+			});
+			break;
+		
+		case "spotify-this-song":
+			console.log("SPOTIFY");
+			spotify.search({ type: 'track', query: 'Real Slim Shady', limit: 1}, function (err, data){
+			if (err) {
+    		return console.log('Error occurred: ' + err);
+  			}
+  			else{
+  			console.log(data.tracks.items[0].artists[0].name);
+			console.log(data.tracks.items[0].name);
+			console.log(data.tracks.items[0].album.external_urls.spotify);
+			console.log(data.tracks.items[0].album.name);
+  				}
+ 			});
 
-	// 	case "spotify-this-song":
-	// 	console.log("SPOTIFY");
-	// 	spotify.search()
-	// 	break;
+			break;
 
-	// 	case "movie-this":
-	// 	console.log("MOVIE");
-	// 	break;
-	//	 };
+		case "movie-this":
+			console.log("MOVIE");
+			break;
+	 };
+
 	// * `my-tweets`
 
     // * `spotify-this-song`
